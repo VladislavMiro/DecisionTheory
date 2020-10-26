281bd25bd29ba71cfe1b340b6f9ebf778bf3871a
Vladislav <vladislavmiro@icloud.com>
2020-10-26 19:44:56 +0300

    lab1 improvements

--- lab1/lab1_1/src/com/company/Main.java
+++ lab1/lab1_1/src/com/company/Main.java
@@ -1,7 +1,6 @@
 package com.company;
 
 import java.util.Arrays;
-import java.util.Scanner;
 
 public class Main {
 
@@ -16,13 +15,13 @@ public class Main {
 
 
         outputMatrix(matrix, "Матрица доходов:");
-        System.out.printf("Z = %.3f\n", waldMethod(matrix));
-        System.out.printf("Z = %.3f\n", savageMethod(matrix));
-        System.out.printf("Z = %.3f\n", gurvisMethod(matrix));
+        System.out.println("Номер строки: "+waldMethod(matrix)+"\n");
+        System.out.println("Номер строки: "+savageMethod(matrix)+"\n");
+        System.out.println("Номер строки: "+gurvisMethod(matrix)+"\n");
 
         System.out.println("Задание 2.\n");
 
-        double[][] probabilityMatrix =  {{100,150,200,250,300},{0.2,0.25,0.3,0.15,0.1}};
+        double[][] probabilityMatrix =  {{100,150,200,250,300},{0.15,0.4,0.3,0.1,0.05}};
 
         double[][] answerMat = getResultMatrix(probabilityMatrix);
 
@@ -36,29 +35,46 @@ public class Main {
 
 
     }
-
+//процедура вывода матрицы в консоль
     private static void outputMatrix(double[][] matrix, String message) {
         System.out.println(message);
-        for (double[] row:matrix) {
-            for (double element: row) {
-                System.out.printf("%.3f\t", element);
+        for (double[] rows: matrix) {
+            for (double element: rows) {
+                System.out.printf("%.3f | ", element);
             }
             System.out.println();
         }
         System.out.println();
     }
-
+//процедура вывода вектора в консоль
     private static void printVector(double[] vector) {
         System.out.print("[");
-        for (double element : vector) {
-            System.out.printf("%.3f, ",element);
+        for (int i = 0; i < vector.length; i++) {
+            if(i != vector.length-1) {
+                System.out.printf("%.3f, ", vector[i]);
+            } else {
+                System.out.printf("%.3f", vector[i]);
+            }
         }
         System.out.print("]");
         System.out.println();
     }
+//Функция поиска индекса
+    private static int findIndexInArray(double[] array, double element) {
+        int index = -1;
+        for (int i = 0; i < array.length; i++) {
+            if(array[i] == element) {
+                index = i;
+                break;
+            }
+        }
+
+        return ++index;
+    }
 //----------------Задание 1-------------------------------
+    //реализация минимаксного критерия
     private static double waldMethod(double[][] matrix) {
-        System.out.println("Минимаксный метод:");
+        System.out.println("Минимаксный критерий:");
         double[] tmp = new double[matrix.length];
 
         for (int i = 0; i < matrix.length; i++)
@@ -66,11 +82,15 @@ public class Main {
 
         printVector(tmp);
 
-        return Arrays.stream(tmp).max().getAsDouble();
+        double max = Arrays.stream(tmp).max().getAsDouble();
+
+        System.out.printf("Z = %.3f\n", max);
+
+        return findIndexInArray(tmp,max);
     }
 //------------------------Метод Сэвиджа (S)---------------------------------------------
     private static double savageMethod(double[][] matrix) {
-        System.out.println("Метод Сэвиджа (S):");
+        System.out.println("Критерий Сэвиджа (S):");
 
         double[][] deltaK = new double[matrix.length][matrix[0].length];
         double[] k = new double[matrix.length];
@@ -83,10 +103,12 @@ public class Main {
             k[i] = Arrays.stream(deltaK[i]).max().getAsDouble();
 
         printVector(k);
+        double min = Arrays.stream(k).min().getAsDouble();
+        System.out.printf("Z = %.3f\n", min);
 
-        return Arrays.stream(k).min().getAsDouble();
+        return findIndexInArray(k,min);
     }
-
+    //Получение максимального элемента в столбце матрицы
     private static double getMaxColumn(double[][] matrix, int index) {
         double[] column = new double[matrix.length];
 
@@ -99,7 +121,7 @@ public class Main {
 //------------------------Метод Гурвица (HW)-------------------------------------------
 
     private static double gurvisMethod(double[][] matrix) {
-        System.out.println("Метод гурвица Гурвица (HW):");
+        System.out.println("Критерий Гурвица (HW):");
 
         double[] tmp = new double[matrix.length];
         double c = 0.87;
@@ -108,10 +130,13 @@ public class Main {
             tmp[i] = c * Arrays.stream(matrix[i]).min().getAsDouble() + (1 - c) * Arrays.stream(matrix[i]).max().getAsDouble();
 
         printVector(tmp);
+        double max = Arrays.stream(tmp).max().getAsDouble();
+        System.out.printf("Z = %.3f\n", max);
 
-        return Arrays.stream(tmp).max().getAsDouble();
+        return findIndexInArray(tmp,max);
     }
 ///-----------------------------Задание 2-------------------------------------------
+    //Заполнение таблицы решений (спроса и предложений)
     private static double[][] getResultMatrix(double[][] array){
         double[][] answerMat = new double[array[0].length][array[0].length];
 
@@ -127,9 +152,9 @@ public class Main {
 
         return answerMat;
     }
-
+    //Реализация критерия Байеса-Лапласа
     private static int bayesLaplaceMethod(double[][] resultMatrix, double[] probabilityVector) {
-        System.out.println("Метод Байеса-Лапласа:");
+        System.out.println("Критерий Байеса-Лапласа:");
         double[] e = new double[probabilityVector.length];
 
         for (int i = 0; i < resultMatrix.length; i++) {
@@ -142,15 +167,7 @@ public class Main {
         double max = Arrays.stream(e).max().getAsDouble();
         System.out.printf("Z = %.3f\n", max);
 
-        int index = -1;
-        for (int i = 0; i < e.length; i++) {
-            if(e[i] == max) {
-                index = i;
-                break;
-            }
-        }
-
-        return index;
+        return findIndexInArray(e, max);
     }
 
 
